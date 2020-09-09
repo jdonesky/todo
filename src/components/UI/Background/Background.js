6a89e2b7ce63a6bca199484cb778c7f8a50878cb
Jon <jdonesky@gmail.com>
2020-09-09 11:49:36 -0400

    added background

---
+++ src/components/UI/Background/Background.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import classes from './Background.module.css'
+
+export const Background = (props) => (<div className={classes.Background}></div>)
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import {Background} from '../../components/UI/Background/Background'
 import { Title } from '../../components/Header/Title/Title.js'
 import classes from './Layout.module.css'
 
 export const Layout = props => {
     return (
+        <React.Fragment>
+        <Background />
         <div className={classes.Layout}>
             <Title title="todos" />
             <div className={classes.Content}>
                 {props.children}
             </div>
         </div>
+        </React.Fragment>
     )
 }
