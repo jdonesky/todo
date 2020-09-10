dff372f3d80fd46e07a9fc97efc63030c926becc
Jon <jdonesky@gmail.com>
2020-09-10 14:08:52 -0400

    added check icon and set up to add check styling

--- src/components/TodoItems/TodoItem/TodoItem.js
+++ src/components/TodoItems/TodoItem/TodoItem.js
@@ -2,9 +2,15 @@ import React from 'react';
 import classes from './TodoItem.module.css'
 
 const todoItem = (props) => {
+
+    const iconClass = [classes.Icon]
+    if (props.complete) {
+        iconClass.push(classes.Checked);
+    }
+
     return (
         <div className={classes.Container}>
-            <span></span>
+            <span className={iconClass.join(" ")}></span>
             <li className={classes.Item}>{props.entry}</li>
         </div>
     );
